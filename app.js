f417a3714ddd928374b6da8d62c9bed8b7b84316
ferradino <nicholasferradino@icloud.com>
2024-10-23 19:30:57 -0400

    started making form

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const logger = require("morgan");
 
 // Create the express app
 const app = express();
 
+// Get path to page files used in this app (regardless of OS)
+// Store as a static variable in Express
+const pagesPath = path.resolve(__dirname, "pages");
+app.use(express.static(pagesPath));
+
+// Create a logger for request sent to the app
 app.use("/", logger("short"));
 
-// Home page at "/"
-app.get("/", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<html><body><h2>Hello, World!</h2></body></html>');
+// Order form page at "/"
+app.get("/", function(request, response) {
+    response.sendFile("index.html", {root: pagesPath});
 });
 
 // Have the app listen on port 3000
 http.createServer(app).listen(3000);
-
-
