f81aed90793a2b9296a07c0361da607b0fad6525
ferradino <nicholasferradino@icloud.com>
2024-10-23 20:22:50 -0400

    finsihed ordering page and route structure inside of the app

--- app.js
+++ app.js
@@ -19,5 +19,43 @@ app.get("/", function(request, response) {
     response.sendFile("index.html", {root: pagesPath});
 });
 
+// Recieved an order at "/order"
+// 1. Parse order information, create entity object for order
+app.get("/order", function(request, response, next) {
+    next();
+});
+
+// 2. Validate that order information is valid
+app.get("/order", function(request, response, next) {
+    next();
+});
+
+// 3. Compute price of the order
+app.get("/order", function(request, response, next) {
+    next();
+});
+
+// 4. Display the order and total cost
+app.get("/order", function(request, response) {
+});
+
+
+// The order was invalid, send error message back to user
+app.use("/order", function(err, request, response, next) {
+});
+
+// Request not handled by any previous gets, so send error page
+app.use(function(request, response) {
+    response.writeHead(404, {'Content-Type': 'text/html'});
+    response.end('<html><body><h2>Sorry -- file not found!</h2></body></html>');
+});
+
+// Error was not handled, so send server error page as a last resort
+app.use(function(err, request, response, next) {
+    console.log(err);
+    response.writeHead(500, {'Content-Type': 'text/html'});
+    response.end('<html><body><h2>Server error!</h2></body></html>');
+});
+
 // Have the app listen on port 3000
 http.createServer(app).listen(3000);
