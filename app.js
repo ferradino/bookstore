ba6727dac692ebb208346c16bc2a102d4096bf80
ferradino <nicholasferradino@icloud.com>
2024-10-24 17:13:46 -0400

    added all application features

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const logger = require("morgan");
 // Create the express app
 const app = express();
 
+// Import the model functions to be used in the application 
+const support = require("./models/model");
+
 // Get path to page files used in this app (regardless of OS)
 // Store as a static variable in Express
 const pagesPath = path.resolve(__dirname, "pages");
@@ -22,27 +25,49 @@ app.get("/", function(request, response) {
 // Recieved an order at "/order"
 // 1. Parse order information, create entity object for order
 app.get("/order", function(request, response, next) {
+    request.ISBN = request.query.ISBN;
+    request.quantity = request.query.quantity;
     next();
 });
 
 // 2. Validate that order information is valid
 app.get("/order", function(request, response, next) {
+    request.errors = support.validateOrder(request.ISBN, request.quantity);
+
+    if (request.errors.length > 0) {
+        next(new Error("validation"));
+    }
     next();
 });
 
 // 3. Compute price of the order
 app.get("/order", function(request, response, next) {
+    request.book = support.getBookByISBN(request.ISBN);
+    request.bill = support.computeBill(request.book.price, request.quantity);
     next();
 });
 
 // 4. Display the order and total cost
 app.get("/order", function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('<html><body>Thank you for your order of ' + request.quantity + ' copies of <b>' + request.book.title + '</b>! At $' + request.book.price + ' per copy, your total bill is $' + request.bill.toFixed(2) + ' with tax.</body></html>');
 });
 
 
 // The order was invalid, send error message back to user
 app.use("/order", function(err, request, response, next) {
+    if (err.message.includes("validation")) {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        var message = "<html><body><p>The following errors were found:<ul>"
+        for (let error of request.errors) {
+            message += "<li>"+ error +"</li>";
+        }    
+        response.end(message+'</ul> Press the BACK button and try again!</p></body></html>');
+    } else {
+        next(err);
+    }
 });
+
 
 // Request not handled by any previous gets, so send error page
 app.use(function(request, response) {
@@ -52,7 +77,6 @@ app.use(function(request, response) {
 
 // Error was not handled, so send server error page as a last resort
 app.use(function(err, request, response, next) {
-    console.log(err);
     response.writeHead(500, {'Content-Type': 'text/html'});
     response.end('<html><body><h2>Server error!</h2></body></html>');
 });
--- models/model.js
+++ models/model.js
@@ -1,1 +1,51 @@
+// Get list of books available for purchase
+function getBookList() {
+    const bookList = [
+        {ISBN: "1368098168", title: "The Lightning Theif", price: 8.99, imageFile: "./images/978-1368098168.png"},
+        {ISBN: "1423146780", title: "Blood of Olympus", price: 10.99, imageFile: "./images/978-1423146780.png"},
+        {ISBN: "1423163374", title: "The Sword of Summer", price: 9.99, imageFile: "./images/978-1423163374.png"},
+    ];
 
+    return bookList;
+}
+
+// Search the given ISBN and see if it matches one of the books
+// If the book was found in the list of available books return true
+// If not, return false indicating the books is not available
+function getBookByISBN(ISBN) {
+    var bookList = getBookList();
+    for (let book of bookList) {
+        if (book.ISBN === ISBN) {
+            return book;
+        }
+    }
+}
+
+
+// Validate the order
+// The checks if all the input data from the html form is valid
+// Will generate and return errors if any of the order elements is invalid
+function validateOrder(ISBN, quantity) {
+    var errors = [];
+    if (ISBN === 0) {
+        errors.push("Must select a book!"); 
+    }
+    if (isNaN(quantity) || quantity < 1) {
+        errors.push("Must purchase at least one copy!");
+    }
+    return errors;
+}
+
+
+// Compute the bill of the order
+// Tax: 1.75%
+// Total Price is price of book * quantity * tax
+function computeBill(price, numCopies) {
+    const tax = 1.0175
+    var bill = price * numCopies * tax;
+    return bill;
+}
+
+
+// export all the module function so that they can be used throughout application
+module.exports = {getBookList, getBookByISBN, validateOrder, computeBill};
